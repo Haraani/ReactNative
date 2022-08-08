fb36d6bd3910a5d49d6ac8e47164ba469bd5d84d
Haraani <anirudh.h92@gmail.com>
2022-08-08 15:47:51 +0530

    Flex_Layout_Branch

--- nebula-app/App.js
+++ nebula-app/App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goals" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.inputContainer}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            width: 100,
+            height: 100,
+            alignItems: 'center',
+          }}
+        >
+          <Text> Spray </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            width: 100,
+            height: 100,
+            alignItems: 'center',
+          }}
+        >
+          <Text> Foot Trigger </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'pink',
+            width: 100,
+            height: 100,
+            alignItems: 'center',
+          }}
+        >
+          <Text> Approach </Text>
+        </View>
+      </View>
+      <View style={{flexDirection: 'row-reverse', justifyContent:'space-between'}}>
+        <Button color="red" title="Order on Amazon" disabled/>
+        <Button title="Order on Nebula" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    padding: 10,
   },
 });
